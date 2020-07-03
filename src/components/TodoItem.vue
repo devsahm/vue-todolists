a8dc35f5f30bd1272a1a3734085a92ca4ae761e0
Fagbenro Damilare Samuel <fagbenrosamuel@gmail.com>
2020-07-03 15:42:48 +0100

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p>{{todo.title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TodoItem",
+    props:["todo"]
+    
+}
+</script>
+
+
+<style scoped>
+.todo-item{
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+
+}
+
+.is-completed{
+    text-decoration: underline;
+
+}
+
+.del{
+    background: #ff0000;
+    color: #fff;
+    border:none;
+    padding: 5px 9px;
+    cursor: pointer;
+    float:right;
+}
+
+</style>
